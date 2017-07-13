72091ea5ff77858210c8736dfdca1ee38bdc3519
alumno <alumno@DELGADOBRENDAJUDITH>
2017-07-13 13:17:51 -0300

    ticket 15

--- PHumanidades-war/src/java/Beans/ConsultaCobrosGeneralesBean.java
+++ PHumanidades-war/src/java/Beans/ConsultaCobrosGeneralesBean.java
@@ -18,21 +18,35 @@ import com.lowagie.text.BadElementException;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
 import com.lowagie.text.Image;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFRow;
@@ -48,8 +62,12 @@ import org.apache.poi.hssf.util.HSSFColor;
 @ViewScoped
 public class ConsultaCobrosGeneralesBean implements Serializable {
 
+     private final String escudo1 = FacesContext.getCurrentInstance().getExternalContext().getRealPath("\\Imagenes\\escudo.jpg");
+     private final String escudo2 = FacesContext.getCurrentInstance().getExternalContext().getRealPath("\\Imagenes\\logo2.jpg");
     @EJB
     private IngresoRNLocal cobroGeneralRNLocal;
+    @ManagedProperty(value = "#{cohorteLstBean}")
+    private CohorteLstBean cohorteLstBean;
 
     private List<Ingreso> lstCobroGeneral;
     private Date fechaFin;
@@ -84,6 +102,14 @@ public class ConsultaCobrosGeneralesBean implements Serializable {
 
     public void setFechaIni(Date fechaIni) {
         this.fechaIni = fechaIni;
+    }
+
+    public CohorteLstBean getCohorteLstBean() {
+        return cohorteLstBean;
+    }
+
+    public void setCohorteLstBean(CohorteLstBean cohorteLstBean) {
+        this.cohorteLstBean = cohorteLstBean;
     }
 
     public IngresoRNLocal getCobroGeneralRNLocal() {
@@ -213,5 +239,69 @@ public class ConsultaCobrosGeneralesBean implements Serializable {
         //     this.setLstPagosDocente(new ArrayList<PagosDocente>());
         RequestContext.getCurrentInstance().update("frmPri:dtCobrosGenerales");
     }
+   
+    public void preProcessPDF2(Object document) throws IOException, BadElementException, DocumentException {
+        Document pdf = (Document) document;
+        pdf.open();
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        String logo = externalContext.getRealPath("") + File.separator + "Imagenes" + File.separator + "LogoFacultadHumanidades70x70.png";
+        //SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        // pdf.add(new Phrase("Fecha: " + formato.format(new Date())));
+        
+        PdfPTable pdfTable = new PdfPTable(6);
+        PdfPCell pdfcell = new PdfPCell();
+        pdfcell.setColspan(1);
+        pdfcell.setImage(Image.getInstance(logo));
+	pdfTable.addCell(pdfcell);
+        final Phrase phrase = new Phrase("FACULTAD DE HUMANIDADES \n última cuota alumnos \n "+ 
+                new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+        phrase.setFont(FontFactory.getFont(FontFactory.TIMES_ROMAN));
+        PdfPCell pdfcell2 = new PdfPCell();
+        pdfcell2.setColspan(5);
+        pdfcell2.setHorizontalAlignment(1);
+        pdfcell2.setVerticalAlignment(1);
+        pdfcell2.setPhrase(phrase);
+        pdfTable.addCell(pdfcell2);
+    
+	pdf.add(pdfTable);
+    }
+    
+    
+     public void generar() throws SQLException {
 
+        Connection conect;
+        conect = DriverManager.getConnection("jdbc:postgresql://localhost:5432/humanidades", "postgres", "123456");
+        String path;
+        System.out.println("funcionando");
+
+        try {
+
+            HashMap parametros = new HashMap();
+            
+           
+            parametros.put("escudo1",escudo1 );
+            parametros.put("escudo2",escudo2 );
+            parametros.put("cohorte",this.getCohorteLstBean().getCohorteSelect().getDescripcion() );
+            parametros.put("cohorteID",this.getCohorteLstBean().getCohorteSelect().getId());
+            path = "reporte\\alumnoPorCohorte.jasper";
+//funcionando
+
+            FacesContext context = FacesContext.getCurrentInstance();
+            String reportPath = context.getExternalContext().getRealPath(path);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(reportPath, parametros, conect); //new JREmptyDataSource() si le pongo eso en vez de conect me devuelve null
+            HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+            httpServletResponse.addHeader("Content-disposition", "filename=reporte.pdf");
+            ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream();
+            JasperExportManager.exportReportToPdfStream(jasperPrint, servletOutputStream);
+            servletOutputStream.flush();
+            servletOutputStream.close();
+            FacesContext.getCurrentInstance().responseComplete();
+
+        } catch (Exception ex) {
+            System.out.println(ex + "CAUSA: " + ex.getCause());
+
+        }
+
+    }//fin generar
+    
 }
